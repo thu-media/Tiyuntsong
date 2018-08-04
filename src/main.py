28427c74cd89d1b8d51cc0b61e2772b761f89abc
godka <htc17@mails.tsinghua.edu.cn>
2018-08-04 15:42:07 +0800

    cool

--- src/main.py
+++ src/main.py
@@ -5,28 +5,38 @@ from network import Zero
 m_a = 0
 m_b = 0
 m_c = 0
+log_file = open('zero.txt','w')
+
 def battle(agent_result):
-    global m_a, m_b, m_c
-    if m_b != 0:
-        print(round(m_a * 100.0 / m_c, 2), round(m_b * 100.0 / m_c, 2), round((m_c - m_a - m_b) * 100.0 / m_c, 2))
+    global m_a, m_b, m_c, log_file
     total_bitrate0, total_rebuffer0, _ = agent_result[0]
     total_bitrate1, total_rebuffer1, _ = agent_result[1]
-    #print(total_bitrate0,total_bitrate1,total_rebuffer0,total_rebuffer1)
+    if m_c > 0:
+        print(round(m_a * 100.0 / m_c, 2), round(m_b * 100.0 / m_c, 2))
+        print(total_bitrate0,total_rebuffer0,total_bitrate1,total_rebuffer1)
+        log_file.write(str(round(m_a * 100.0 / m_c, 2)) + ',' + str(total_bitrate0) + ',' + str(total_rebuffer0) + ',' + str(total_bitrate1) + ',' + str(total_rebuffer1))
+        log_file.write('\n')
+        log_file.flush()
+#    total_bitrate0, total_rebuffer0, _ = agent_result[0]
+#    total_bitrate1, total_rebuffer1, _ = agent_result[1]
+    #print(total_bitrate0,total_rebuffer0,total_bitrate1,total_rebuffer1)
     m_c += 1
     if total_rebuffer0 == total_rebuffer1 and total_bitrate0 == total_bitrate1:
         return [0,0]
-    if total_rebuffer0 <= total_rebuffer1:
-        if total_bitrate0 >= total_bitrate1:
+    if total_rebuffer0 < total_rebuffer1:
+        m_a += 1
+        return [1, -1]
+    if total_rebuffer0 == total_rebuffer1:
+        if total_bitrate0 > total_bitrate1:
             m_a += 1
             return [1, -1]
         else:
-            return [0, 0]
-    else:
-        if total_bitrate0 < total_bitrate1:
             m_b += 1
             return [-1, 1]
-        else:
-            return [0, 0]
+    else:
+        m_b += 1
+        return [-1, 1]
+
 
 
 def main():
@@ -45,3 +55,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
