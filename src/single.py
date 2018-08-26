e61142c7f9a103914b650577285737463275bca5
godka <mythkast@hotmail.com>
2018-08-26 20:12:33 +0800

    rename

--- src/single.py
+++ src/single.py
@@ -1,56 +1,73 @@
 import sabre as env
 import math
 from network import Zero
-
-m_a = 0
-m_b = 0
-m_c = 0
-log_file = open('zero.txt','w')
-
-def battle(agent_result):
-    global m_a, m_b, m_c, log_file
-    total_bitrate0, total_rebuffer0, _ = agent_result[0]
-    total_bitrate1, total_rebuffer1, _ = agent_result[1]
-    if m_c > 0:
-        print(round(m_a * 100.0 / m_c, 2), round(m_b * 100.0 / m_c, 2))
-        print(total_bitrate0,total_rebuffer0,total_bitrate1,total_rebuffer1)
-        log_file.write(str(round(m_a * 100.0 / m_c, 2)) + ',' + str(total_bitrate0) + ',' + str(total_rebuffer0) + ',' + str(total_bitrate1) + ',' + str(total_rebuffer1))
-        log_file.write('\n')
-        log_file.flush()
-#    total_bitrate0, total_rebuffer0, _ = agent_result[0]
-#    total_bitrate1, total_rebuffer1, _ = agent_result[1]
-    #print(total_bitrate0,total_rebuffer0,total_bitrate1,total_rebuffer1)
-    m_c += 1
-    if total_rebuffer0 < total_rebuffer1:
-        m_a += 1
-        return [1, 0]
-    elif total_rebuffer0 == total_rebuffer1:
-        if total_bitrate0 > total_bitrate1:
-            m_a += 1
-            return [1, 0]
-        elif total_bitrate0 == total_bitrate1:
-            return [0, 0]
-        else:
-            m_b += 1
-            return [0, 1]
-    else:
-        m_b += 1
-        return [0, 1]
-
+from tracepool import tracepool
+import numpy as np
+from tqdm import tqdm
+from rules import rules
+from log import log
+import os
+NUM_AGENT = 2
 
 
 def main():
-    agent_list = [Zero('A'), Zero('B')]
+    _log = log('zero.txt')
+    #log_file = open('zero.txt', 'w')
+    elo_file = open('elo.txt', 'w')
+    agent_list = []
+    agent_elo = []
+    for p in range(NUM_AGENT):
+        agent_list.append(Zero(str(p)))
+        agent_elo.append(1000.0)
+    _tracepool = tracepool(ratio=0.1)
     while True:
-        agent_result = []
+        _tmp = [0, 0, 0]
+        _state_stack, _reward_stack = [], []
+        _trace_result = []
+        for _trace in tqdm(_tracepool.get_list(),ascii=True):
+            agent_result = []
+            for _agent in agent_list:
+                total_bitrate, total_rebuffer, total_smoothness = env.execute(
+                    abr=_agent, trace=_trace)
+                agent_result.append(
+                    (total_bitrate, total_rebuffer, total_smoothness))
+            agent_reward = []
+            for _index in range(len(agent_list[0].quality_history)):
+                res = rules([agent_list[0].quality_history[_index],
+                             agent_list[-1].quality_history[_index]])
+                agent_reward.append(res)
+            agent_reward = np.array(agent_reward)
+            tmp_battle = rules(agent_result)
+            _log.write_log(agent_result)
+            _tmp[np.argmax(tmp_battle)] += 1
+            _tmp[-1] += 1
+            for _index, _agent in enumerate(agent_list):
+                _agent.push(agent_reward[:, _index])
+            _trace_result.append(agent_result)
+        _delta_array = [_tmp[0] / _tmp[-1], _tmp[1] / _tmp[-1]]
+        for _agent, _d in zip(agent_list, _delta_array):
+            _agent.learn(_d)
+        # _clear = np.argmax(_tmp[0:-1])
+        # _update = np.argmin(_tmp[0:-1])
+        # agent_list[_update].teach(agent_list[_clear].pull())
         for _agent in agent_list:
-            total_bitrate, total_rebuffer, total_smoothness = env.execute(
-                abr=_agent, argv='-nm 0.1')
-            agent_result.append((total_bitrate, total_rebuffer, total_smoothness))
-        agent_reward = battle(agent_result)
-
-        for _agent, _r in zip(agent_list, agent_reward):
-            _agent.update(_r)
+            _agent.clear()
+        
+        agent_elo = []
+        for p in range(NUM_AGENT):
+            agent_elo.append(1000.0)
+        _rate, agent_elo = _tracepool.battle(agent_elo, _trace_result)
+        for p in agent_elo:
+            elo_file.write(str(p) + ' ')
+        elo_file.write('\n')
+        elo_file.flush()
+        print(_rate)
+        print(agent_elo)
+        print(round(_tmp[0] * 100.0 / _tmp[-1], 2), '%',
+              ',', round(_tmp[1] * 100.0 / _tmp[-1], 2), '%')
+              
+        _log.write_line()
+        os.system('python draw.py')
 
 
 if __name__ == '__main__':
